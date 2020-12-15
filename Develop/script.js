7505496ea78c6949486551345c0fc789bf7d4074
Kolton Decker <deckerman25@gmail.com>
2020-12-14 18:24:07 -0600

    Added a looping prompt for passsword length if input does not fall between desired boundaries and initialized if statements for password generating function.

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 //Prompts user with password criteria
 //var passwordCriteria = alert("Passwords must: Be a minimum of eight (8) characters in length. Be memorized; if a password is written down it must be secure. Contain at least one (1) character from three (3) of the following categories: Uppercase letter (A-Z). Lowercase letter (a-z). Digit (0-9). Special character (~`!@#$%^&*()+=_-{}[]\|:;”’?/<>,.). Be private. Passwords must not: Contain a common proper name, login ID, email address, initials, first, middle or last name. Have been used in the last year or last three passwords, whichever is greater.")
-var passwordLength = prompt("Please pick a password length between 8 and 128 characters long.");
+do {
+  var passwordLength = prompt("Please pick a password length between 8 and 128 characters long.");
+}
+while (passwordLength < 8 || passwordLength > 128);
 var lowercase = confirm("Would you like to include lowercase letters?");
 var uppercase = confirm("Would you like to include uppercase letters?");
 var numbers = confirm("Would you like to include numbers?");
@@ -13,6 +16,23 @@ var specialChars = confirm("Would you like to include special characters?");
 function writePassword(passwordLength, lowercase, uppercase, numbers, specialChars) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  for (i = 0; i < passwordLength; i++) {
+    if (lowercase) {
+
+    }
+    if (uppercase) {
+
+    }
+    if (numbers) {
+
+    }
+    if (specialChars) {
+
+    }
+  }
+
+  
 
   passwordText.value = password;
 
