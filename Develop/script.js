f3a3cb067cdbf5bb00abb4237d46c79c7ae857d1
Kolton Decker <deckerman25@gmail.com>
2020-12-14 19:22:03 -0600

    Added arrays for all criteria to use later to generate random password.

--- Develop/script.js
+++ Develop/script.js
@@ -14,10 +14,15 @@ var specialChars = confirm("Would you like to include special characters?");
 
 // Write password to the #password input
 function writePassword(passwordLength, lowercase, uppercase, numbers, specialChars) {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var criteria = [];
+  var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var specialCharsArray = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', ':', ';', '<', '=', '>', '?', '@', '^', '_', '|', '~'];
 
-  for (i = 0; i < passwordLength; i++) {
+  var password = generatePassword() {
+    for (i = 0; i < passwordLength; i++) {
+    var randomPassword = Math.floor(Math.random() * criteria.length);
     if (lowercase) {
 
     }
@@ -31,10 +36,10 @@ function writePassword(passwordLength, lowercase, uppercase, numbers, specialCha
 
     }
   }
+};
+var passwordText = document.querySelector("#password");
 
-  
-
-  passwordText.value = password;
+passwordText.value = password;
 
 }
 
