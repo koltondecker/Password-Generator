a48a81fb48c83f4e4619371354ea664cd9cc8f74
Kolton Decker <deckerman25@gmail.com>
2020-12-14 18:06:29 -0600

    Added prompt/confirm messages for password criteria input from user

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Prompts user with password criteria
+//var passwordCriteria = alert("Passwords must: Be a minimum of eight (8) characters in length. Be memorized; if a password is written down it must be secure. Contain at least one (1) character from three (3) of the following categories: Uppercase letter (A-Z). Lowercase letter (a-z). Digit (0-9). Special character (~`!@#$%^&*()+=_-{}[]\|:;”’?/<>,.). Be private. Passwords must not: Contain a common proper name, login ID, email address, initials, first, middle or last name. Have been used in the last year or last three passwords, whichever is greater.")
+var passwordLength = prompt("Please pick a password length between 8 and 128 characters long.");
+var lowercase = confirm("Would you like to include lowercase letters?");
+var uppercase = confirm("Would you like to include uppercase letters?");
+var numbers = confirm("Would you like to include numbers?");
+var specialChars = confirm("Would you like to include special characters?");
+
 // Write password to the #password input
-function writePassword() {
+function writePassword(passwordLength, lowercase, uppercase, numbers, specialChars) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
