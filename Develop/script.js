58565f95a940379e4c1282997fa941ccf32daf11
Kolton Decker <deckerman25@gmail.com>
2020-12-15 14:39:04 -0600

    Added password generating functionality by implementing an empty string that gets a random character from the criteria array added to it each iteration the desired length of the password.

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,6 @@
 var generateBtn = document.querySelector("#generate");
 
 //Prompts user with password criteria
-//var passwordCriteria = alert("Passwords must: Be a minimum of eight (8) characters in length. Be memorized; if a password is written down it must be secure. Contain at least one (1) character from three (3) of the following categories: Uppercase letter (A-Z). Lowercase letter (a-z). Digit (0-9). Special character (~`!@#$%^&*()+=_-{}[]\|:;”’?/<>,.). Be private. Passwords must not: Contain a common proper name, login ID, email address, initials, first, middle or last name. Have been used in the last year or last three passwords, whichever is greater.")
 do {
   var passwordLength = prompt("Please pick a password length between 8 and 128 characters long.");
 }
@@ -13,34 +12,45 @@ var numbers = confirm("Would you like to include numbers?");
 var specialChars = confirm("Would you like to include special characters?");
 
 // Write password to the #password input
-function writePassword(passwordLength, lowercase, uppercase, numbers, specialChars) {
+function writePassword() {
   var criteria = [];
   var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var specialCharsArray = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', ':', ';', '<', '=', '>', '?', '@', '^', '_', '|', '~'];
 
-  var password = generatePassword() {
-    for (i = 0; i < passwordLength; i++) {
-    var randomPassword = Math.floor(Math.random() * criteria.length);
-    if (lowercase) {
-
-    }
-    if (uppercase) {
-
-    }
-    if (numbers) {
-
-    }
-    if (specialChars) {
-
-    }
+  //Pushes our various criteria specific arrays to our blank criteria array if user confirms they want them included.
+  if (lowercase) {
+    Array.prototype.push.apply(criteria, lowercaseArray);
   }
-};
-var passwordText = document.querySelector("#password");
+  if (uppercase) {
+    Array.prototype.push.apply(criteria, uppercaseArray);
+  }
+  if (numbers) {
+    Array.prototype.push.apply(criteria, numbersArray);
+  }
+  if (specialChars) {
+    Array.prototype.push.apply(criteria, specialCharsArray);
+  }
 
-passwordText.value = password;
+  //Calls the generatePassword function and saves return value to variable called password.
+  var password = generatePassword(); 
 
+  //Possibly puts the generated password into the text area on the page???
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+  //Function that uses a for loop that iterates the length of the password desired by user.
+  //It generates a random character/letter/number each iteration based on criteria array built earlier and appends to new password array.
+  function generatePassword() {
+    var password = "";
+    for (i = 0; i < passwordLength; i++) {
+      var randomCharacter = Math.floor(Math.random() * criteria.length);
+      // Pushes randomCharacter to our password array each iteration.
+      password += criteria[randomCharacter]; 
+    }
+    return password;
+  }
 }
 
 // Add event listener to generate button
